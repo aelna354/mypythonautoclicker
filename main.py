89794b2e22b31d5f4ea7463649e27ac9465a1ade
aelna354 <elnasrianconcept@gmail.com>
2018-04-20 17:43:24 -0600

    Making progress thanks to the keyboard package

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import pyautogui
 import time
 import webbrowser
 
--- test.py
+++ test.py
@@ -1,2 +1,23 @@
 #Created for testing.
-#The goal of this program: print X when active and stop when not.
+#The goal of this program: whenever CTRL is held while program is active,
+#rapid fire print X.
+
+from tkinter import *
+import pyautogui
+import keyboard
+import time
+
+class App(Frame):
+	def __init__(self, master):
+		Label(master,text="Press F1 or the start button, and we begin!").grid(row=0,column=0,sticky=W)
+		Label(master,text="Press F2 or press stop, and we stop.").grid(row=1,column=0,sticky=W)
+		keyboard.add_hotkey("F3", self.press)
+	def press(self):
+		time.sleep(0.3)
+		print("Hi!")
+
+program = Tk()
+program.title("The test program")
+program.geometry("400x250")
+app = App(program)
+program.mainloop()
