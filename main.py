e1c14e492b162f8afdd572b47bae271b697cace3
aelna354 <elnasrianconcept@gmail.com>
2018-04-20 23:58:50 -0600

    Will test this functioning build in Choices soon.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from tkinter import * #gui
-from tkinter import messagebox #help message
-import keyboard #hotkeys
-import pyautogui #click action
-import webbrowser #hyperlink
+#credits to https://github.com/boppreh/keyboard and https://github.com/boppreh/mouse
+#App for being an autoclicker.
+
+from tkinter import *
+from tkinter import messagebox
+import keyboard
+import webbrowser
+import mouse
 
 class App(Frame):
 	def __init__(self, master):
 		self.master = master #here to allow another method to access it
-		self.active = StringVar()
+		self.active = StringVar() #determines if program should be autoclicking
 		self.active.set("INACTIVE")
-		self.speed = IntVar()
+		self.speed = IntVar() #miliseconds between autoclicks
 		self.speed.set(500)
 
 		hyperlink = Label(master, text="Source Code", fg="blue", cursor="hand2")
@@ -29,17 +32,19 @@ class App(Frame):
 		self.status.grid(row=3,column=0)
 
 		actionframe = Frame()
-		Button(actionframe, text="Start (F3)", width=15, bg="silver", fg="Green", command=self.activate).grid(row=0,column=0,sticky=W)
-		Button(actionframe, text="Stop (F4)", width=15, bg="silver", fg="Red", comman=self.stop).grid(row=0,column=1,sticky=W)
+		Button(actionframe, text="Start (F3)", width=15, bg="silver", fg="green", command=self.activate).grid(row=0,column=0,sticky=W)
+		Button(actionframe, text="Stop (F4, RMB)", width=15, bg="silver", fg="red", comman=self.stop).grid(row=0,column=1,sticky=W)
 		actionframe.grid(row=4,column=1,sticky=W)
 
 		keyboard.add_hotkey("F3", self.activate)
 		keyboard.add_hotkey("F4", self.stop)
+		mouse.on_button(self.stop, buttons='right')
 
-		Button(master, text="How To Use", command=lambda: self.help()).grid(row=5,column=0,sticky=W)
+		Button(master, text="How To Use", command=lambda:self.help()).grid(row=5,column=0,sticky=W)
 
 	def activate(self):
 		if not (self.active.get() == "ACTIVE"):
+			#we don't want to change speed WHILE active so we set it once
 			self.truespeed = self.speed.get()
 			self.active.set("ACTIVE")
 			self.status["fg"] = "green"
@@ -47,7 +52,7 @@ class App(Frame):
 
 	def start(self):
 		if self.active.get() == "ACTIVE":
-			pyautogui.click()
+			mouse.click()
 			print("Click.") #for debugging
 			self.master.after(self.truespeed, self.start)
 
@@ -55,12 +60,13 @@ class App(Frame):
 		if self.active.get() == "ACTIVE":
 			self.active.set("INACTIVE")
 			self.status["fg"] = "red"
+			print("Done.")
 
 	def help(self):
 		messagebox.showinfo("Hi!","Select how many seconds are between clicks."+
 							" (For example, with 0.5, you have a half second between clicks, or two clicks a second.)\n"+
 							"To enable clicking, either press F3 or hit the Start button.\n"+
-							"To disable, either press F4 or hit the Stop button.\n\n"+
+							"To disable, either hit the RMB, press F4, or hit the Stop button.\n\n"+
 							"(NOTE: You cannot change the speed while the program is active. You must first disable it before changing speed.)\n")
 
 program = Tk()
