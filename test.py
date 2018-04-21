42ac90cd0503cff6fbd180610b85754dc7a021b0
aelna354 <elnasrianconcept@gmail.com>
2018-04-20 19:06:00 -0600

    Test.py is functionally working :) Ready for work on main project

--- test.py
+++ test.py
@@ -1,20 +1,36 @@
-#Created for testing.
-#The goal of this program: whenever CTRL is held while program is active,
-#rapid fire print X.
+#Created for testing. The programmer's equivalent of scrap paper... #philosophicalprogrammer
+#This program can be toggled with F3 regardless of window focus.
+#When enabled, the program will periodically print "Yay!"
 
-from tkinter import *
-import pyautogui
-import keyboard
-import time
+from tkinter import * #gui
+import pyautogui	  #clicker
+import keyboard		  #hotkeys
 
-class App(Frame):
+class App(Tk):
 	def __init__(self, master):
-		Label(master,text="Press F1 or the start button, and we begin!").grid(row=0,column=0,sticky=W)
-		Label(master,text="Press F2 or press stop, and we stop.").grid(row=1,column=0,sticky=W)
-		keyboard.add_hotkey("F3", self.press)
-	def press(self):
-		time.sleep(0.3)
-		print("Hi!")
+		self.master = master #used so another method can access master
+		self.active = False
+
+		Label(master, text="Press F3 and we begin!").grid(row=0,column=0,sticky=W)
+		Label(master, text="Press F4 and we stop!").grid(row=1,column=0,sticky=W)
+
+		keyboard.add_hotkey("F3", self.enable)
+		keyboard.add_hotkey("F4", self.disable)
+
+	def start(self):
+		if self.active:
+			print("Yay!")
+			self.master.after(10, self.start)
+
+	def enable(self):
+		if not self.active:
+			self.active = True
+			self.start()
+	
+	def disable(self):
+		if self.active:
+			self.active = False
+			print("Done.")
 
 program = Tk()
 program.title("The test program")
